533699cee5f02aa478ba013feda5752b82a6f4b9
MSSAYED11 <mssayed1108@gmail.com>
2025-10-25 00:34:51 +0530

    deployment

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://sunlo-gold.vercel.app",
   credentials: true
 }));
 
